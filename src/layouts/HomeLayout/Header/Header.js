d6671eac9f2541c39ec787ba1bd0453c88014c8d
daicuong11 <lydaicuong784@gmail.com>
2024-07-19 12:13:50 +0700

    fix some file

--- src/layouts/HomeLayout/Header/Header.js
+++ src/layouts/HomeLayout/Header/Header.js
@@ -14,21 +14,10 @@ const Header = (props) => {
         dispatch(actions.setTheme(theme));
     }
     return (
-        <header className="h-20 bg-red-500">
-            <div className="container flex items-center justify-between h-full mx-auto">
-                <div className="text-lg font-bold text-white">Logo</div>
-                <nav className="space-x-4">
-                    <Link to="/" className="text-white">Home</Link>
-                    <Link to="/about" className="text-white">About</Link>
-                    <Link to="/contact" className="text-white">Contact</Link>
-                    {/* button change theme */}
-                    <Button text >Register</Button>
-                    <Button primary leftIcon={<FontAwesomeIcon icon={faSignIn} />} rightIcon={<FontAwesomeIcon icon={faSignOut} />} >Login</Button>
-                    <button onClick={handleChangeTheme} className="px-3 py-[6px] bg-zinc-300 text-gray-700 dark:bg-zinc-700 dark:text-gray-300 rounded-md">
-                        {theme === 'dark' ? <FontAwesomeIcon className="transition-all rotate-45" size="xl" color="white" icon={faSun} /> : <FontAwesomeIcon className="transition-all" size="xl" color="dark" icon={faMoon} />}
-                    </button>
-                </nav>
-            </div>
+        <header className="h-[48px] bg-gradient-to-r fromh-14 bg-gradient-to-r from-violet-500 to-fuchsia-500">
+            <button onClick={handleChangeTheme} className="px-3 py-[6px] bg-zinc-300 text-gray-700 dark:bg-zinc-700 dark:text-gray-300 rounded-md">
+                {theme === 'dark' ? <FontAwesomeIcon className="transition-all rotate-45" size="xl" color="white" icon={faSun} /> : <FontAwesomeIcon className="transition-all" size="xl" color="dark" icon={faMoon} />}
+            </button>
         </header>
     );
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,13 +2,11 @@
 
 //pages
 import HomePage from "../pages/auth/HomePage";
-import VideoPage from "../pages/auth/VideoPage";
 import Login from "../components/auth/Login";
 
 const publicRoutes = [
     { path: '/', component: HomePage },
     { path: '/login', component: Login, layout: null },
-    { path: '/video', component: VideoPage, layout: null },
 ]
 
 const privateRoutes = [
